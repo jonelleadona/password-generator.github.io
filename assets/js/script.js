808f6c358163dddd2097f7d849713c62f31739eb
Jonelle Adona <jonelle.adona@knights.ucf.edu>
2020-10-01 15:47:21 -0400

    Create generatePassword function
    Add prompts, forloops, and alerts

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,12 @@
 // NOTE: generatePassword() would still work if declared below writePassword() because of how JS works, but still good practice to declare functions before calling them.
 // TODO: Put generatePasword() function here
 
-// Variables
-var confirmNumber = (1, 2, 3, 4, 5, 6, 7, 8, 9);
-var confirmCharacter = ("!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~");
-var confrimLowerCase = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" );
-var confrimUpperCase = ()
  // Add uppercase function 
 
+ // use querySelector when referencing an element in the HTML
  // Add event listener to generate button
- var generateBtn = document.querySelector("#generate");
- generateBtn.addEventListener("click", writePassword);
-
- // Start function
- 
+var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", writePassword);
 
 // TODO: Add logic to prompt user for criterias. Store user's choices in variables.
 // TODO: Based on user's criterias, create a loop to generate each character of the password randomly
@@ -22,13 +15,46 @@ var confrimUpperCase = ()
 // NOTE: generatePassword needs to return the password so the value can be stored in 'var password' in writePassword()
 // NOTE: Look into ASCII table on how it can help with password generation
 
+function generatePassword() 
+{
+  var length = prompt("Please enter password length");
+  var lengthInt = parseInt(length);
+  
+  if (lengthInt < 8 || lengthInt > 128) 
+  {  
+    alert("Your input is invalid");
+    return;
+  }
+ 
+  var useLower = confirm("Do you want to use lowercase characters in password?");
+  var useUpper = confirm("Do you want to use uppercase characters in password?");
+  var useSpecial = confirm("Do you want to use special characters in password?");
+  var useNumeric = confirm("Do you want to use numeric characters in password?");
+
+  if (useLower   !== true &&
+      useUpper   !== true &&
+      useSpecial !== true &&
+      useNumeric !== true) 
+  {
+    alert("At least one of the criterias must be met");
+    return;  
+  }
+  
+  for (var i = 0; i < lengthInt; ++i) 
+  {
+
+  }
+
+}
+
 // Write password to the #password input
-function writePassword() {
+function writePassword() 
+{
   var password = generatePassword(); // TODO: Write generatePassword() function
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
 
+  passwordText.value = password;
 }
 
 
